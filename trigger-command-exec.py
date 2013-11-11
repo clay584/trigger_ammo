9eb77768c07248873a442d2f421db6352e9f7651
Clay Curtis <clay584@gmail.com>
2013-11-11 10:23:31 -0800

    made significant changes to parse sh ip int br

--- trigger-command-exec.py
+++ trigger-command-exec.py
@@ -10,15 +10,35 @@ __version__ = 1.0
 
 from trigger.cmds import Commando
 from trigger.netdevices import NetDevices
-import simplejson as json
 import re
+
+
+def make_dns(hostname, interface, ip_address):
+    print hostname, interface, ip_address
+
+def parse_results(cmdOutput):
+    for key, value in cmdOutput.iteritems():
+        output_lines = str(value).split(r'\n')
+        for i, line in enumerate(output_lines):
+            if i == 0:
+                pass
+            elif 'unassigned' in line:
+                pass
+            elif 'administratively down' in line:
+                pass
+            elif 'up' in line:
+                parsed_items = line.split()
+                make_dns(key, parsed_items[0], parsed_items[1])
+            else:
+                pass
+
 
 nd = NetDevices()
 
 
 class CommandExec(Commando):
     vendors = ['cisco']
-    commands = ['show clock']
+    commands = ['show ip interface brief']
 
     def to_cisco(self, device, commands=None, extra=None):
         print "Sending %r to %s" % (self.commands, device)
@@ -33,17 +53,15 @@ class CommandExec(Commando):
 
 if __name__ == '__main__':
     
+
+    device_list = ['pdc-bar-1.net.jtax.com']
+    """
     device_list = []
     for i in nd:
         dev = nd[i]
         device_list.append(str(dev.nodeName))
-    
+    """ 
     commandExec = CommandExec(devices=device_list)
     commandExec.run() # Commando exposes this to start the event loop
 
-    print '\nResults:'
-    strout = commandExec.results
-    for key, value in commandExec.results.iteritems():
-        newvalue = re.sub(r"^[^:]+ '(.*)'}$",r"\1",str(value))
-        print newvalue
-    
+parse_results(commandExec.results)
